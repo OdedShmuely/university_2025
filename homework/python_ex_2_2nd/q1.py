bcbd8f30701e457c248204843b8d5345eebf7e3b
Oded shmuely <oded.shmuely@gmail.com>
2024-11-28 12:38:47 +0200

    final answer to q1 and q2 + connect tests file

--- homework/python_ex_2_2nd/q1.py
+++ homework/python_ex_2_2nd/q1.py
@@ -1,4 +1,10 @@
-def flip(pile, index):  # flips the pile using a temporary value to help switch the numbers
+"""
+the function will take the index its given and will flip the pile of pancakes upside down
+"""
+def flip(pile, index):
+    if index < 0 or index > len(pile)-1:
+        print ('invalid index, wont change the pile')
+        return pile
     flip_range = index // 2 if index % 2 == 0 else (index + 1) // 2
     for i in range(flip_range):
         temp_value = pile[i]
@@ -7,6 +13,9 @@ def flip(pile, index):  # flips the pile using a temporary value to help switch 
     return pile
 
 
+"""
+the function will find the largest pancake and returns its index in the pile
+"""
 def find_largest_pancake(pile, index):  # finds the largest pancake
     to_index = index
     max_size = 0
@@ -17,7 +26,9 @@ def find_largest_pancake(pile, index):  # finds the largest pancake
             max_size_idx = i
     return max_size_idx
 
-
+"""
+the function will sort the pile and make if look like a pyramid from the largest pancake at the bottom to the smallest on top
+"""
 def pancake_sort(pile):
     for i in range(len(pile)-1,-1,-1):
         flip(pile,find_largest_pancake(pile,i))
@@ -26,26 +37,3 @@ def pancake_sort(pile):
         flip(pile,i)
     return pile
 
-pile1 = [3,1,6,12,17,2,4,9,22]  # Change me!
-pile2 = [5, 2, 11, 15, 8, 1, 13, 20, 10]  # Change me!
-pile3 = flip(pile1, 2)
-print(pile3)
-print(flip(pile2, 3))
-print(find_largest_pancake(pile3, 8))
-print(find_largest_pancake(pile2, 8))
-print(pancake_sort(pile1))
-print(pancake_sort(pile2))
-'''
-def pancake_sort(pile): #a function to sort the pancakes like a pyramid
-    for i in range (len(pile)):
-        flip(pile,find_largest_pancake(pile,len(pile)-i))
-        flip(pile,len(pile))
-        print(pile)
-    return pile
-    idx = len(pile)
-    while idx > 2:
-        flip(pile, find_largest_pancake(pile, idx))
-        flip(pile, idx)
-    return pile
-
-'''
--- homework/python_ex_2_2nd/q2.py
+++ homework/python_ex_2_2nd/q2.py
@@ -1,11 +1,60 @@
 from q1 import *
 
-
+'''
+the function will take the pile and will remove the pancakes in the indexes given 
+and put them im another list
+the function returns the pile of pancakes that were thrown aside
+'''
 def take_pancakes(pile,index_list):
-    pass #delete me and write your code here
+    pile_to_return =[]
+    lst_idx_to_remove = index_list
+    pancake_sort(lst_idx_to_remove)
+    flip(lst_idx_to_remove,len(lst_idx_to_remove)-1)
+    for i in range (len(pile)-1,-1,-1):
+        if len(lst_idx_to_remove)== 0:
+            break
+        if i == lst_idx_to_remove[0]:
+            pile_to_return.append(pile[i])
+            pile.pop(i)
+            lst_idx_to_remove.pop(0)
+        continue
+    flip(pile_to_return,len(pile_to_return)-1)
+    return pile_to_return
 
+'''
+the function will insert pancakes to the pile in the right order to keep it perfect
+the function return none
+'''
 def insert_pancakes(pile,pancakes):
-    pass #delete me and write your code here
+    for i in range (len(pancakes)):
+        if len(pile) == 0:
+            pile.append(pancakes[i])
+        for j in range (len(pile)):
+            if pancakes[i] < pile [j]:
+                pile.insert(j,pancakes[i])
+                break
+            if pancakes[i] > pile [j] and j == len(pile)-1:
+                pile.append(pancakes[i])
+                break
 
-def divide_pancakes(pile,total_size):
-    pass #delete me and write your code here
+'''
+the function will return a canonic list built from pairs that sums up to the 
+total size value given to the function 
+'''
+def divide_pancakes(pile, total_size):
+    if total_size == 1:
+        return 'Cant divide one pancake'
+    lst_pairs = []
+    pile_copy = pile[::]
+    while len(pile_copy) > 0:
+        for i in range (1, len(pile_copy)):
+            if total_size - pile_copy[0] == pile_copy[i]:
+                lst_pairs.append([pile_copy[i], pile_copy[0]])
+                pile_copy.pop(i)
+                break
+        pile_copy.pop(0)
+    if len(lst_pairs) == 0:
+        return 'could not divide pancakes'
+    return lst_pairs
+
+
